40b0b8bab630c383821fc6021bbb2a4dc995215c
unknown <matthewgeddes@yahoo.com>
2018-03-31 00:52:37 -0400

    Save point. About to redo work and might want to come back

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,49 @@
 
+    window.onload = function() {
+        
+    }
+    
     var words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"];
     var wins = 0;
     var losses = 0;
-    var compchoice = words[Math.floor(Math.random()* words.length)];    console.log(compchoice) 
-    var guesses = compchoice.length + 3;
-    var guessesleft = guesses;
-    var guessedletters = []
-
-    function logArray(list) {
-        for (i = 0; i < list.length; i++) { 
-            list[i];
-        }
-    }
-
+    var compChoice = []; 
+    var compChoice = words[Math.floor(Math.random() * words.length)].split('');         console.log(compChoice)
+    var guesses = compChoice.length + 3;
+    var guessesLeft = guesses;
+    var guessedLetters = [];
     var underscores = [];
-        for (u = 0; u < compchoice.length; u++) {
-            underscores = underscores + "_ "
-        };
-    document.getElementById("mysteryword").innerHTML = underscores;
-    document.getElementById("idguessesleft").innerHTML = guessesleft;
-    document.getElementById("idlosses").innerHTML = losses;
-    document.getElementById("idwins").innerHTML = wins;
+    for (u = 0; u < compChoice.length; u++) {
+        underscores = underscores + "_ "
+    };
+
+    
+    
+
+  
    
-      
+
+
+    
     document.onkeyup = function(event) {
         var userGuess = event.key.toLowerCase();
         var thekeycode = event.keyCode;
-        if (guessedletters.includes(userGuess)) {}
+        if (guessedLetters.includes(userGuess)) {}
 
         else if (thekeycode >= 65 && thekeycode <= 90) {    
-            guessedletters.push(userGuess);
-                // if (guesses === 0) {losses++; new compchoice; guessleft = compchoice.length + 2;
+            guessedLetters.push(userGuess);
+                // if (guesses === 0) {losses++; new compChoice; guessleft = compChoice.length + 2;
+                if (compChoice.includes(userGuess)) {
+                    guessesLeft--;
+                }
+
 
                 //          else if (userGuess === indexOf(computerGuess)) {
         }
             
-        var displayguesses = guessedletters.join(', ');   
-        document.getElementById("previousguesses").innerHTML = displayguesses;   
-
+        var displayGuesses = guessedLetters.join(', ');   
+        document.getElementById("previousguesses").innerHTML = displayGuesses;   
+        document.getElementById("idguessesLeft").innerHTML = guessesLeft;
+        document.getElementById("mysteryword").innerHTML = underscores;
         }
     
     
